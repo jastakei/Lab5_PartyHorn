221a8a200bc84f0c9d82ea0ce0118485f770a7f7
jastakei <jas.takei@gmail.com>
2021-02-04 02:26:22 -0800

    adding mute option

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,9 +5,18 @@ var slider = document.getElementById("volume-slider");
 var hornbtn = document.getElementById("honk-btn");
 var hornSound = document.getElementById("horn-sound");
 var image = document.getElementById("volume-image");
+var mainImage = document.getElementById("sound-image");
+var air = document.getElementById("radio-air-horn");
+var car = document.getElementById("radio-car-horn");
+var party = document.getElementById("radio-party-horn");
+
 element.addEventListener("input", updateSlider);
 slider.addEventListener("input", updateNumber);
 hornbtn.addEventListener("click", playSound);
+air.addEventListener("click", makeAir);
+air.addEventListener("click", makeCar);
+air.addEventListener("click", makeParty);
+
 
 
 function updateSlider() {
@@ -15,15 +24,18 @@ function updateSlider() {
    slider.value = element.value;
    hornSound.volume = slider.value/100;
    if (slider.value < 67) {
-       image.src = "https://github.com/jastakei/Lab5_PartyHorn/blob/cc84fdbae756b4be25f02f292bd96f78b867ee78/assets/media/icons/volume-level-2.svg";
+       image.src = "./assets/media/icons/volume-level-2.svg";
    }
-    if (slider.value < 34) {
-        image.src = "https://github.com/jastakei/Lab5_PartyHorn/blob/cc84fdbae756b4be25f02f292bd96f78b867ee78/assets/media/icons/volume-level-1.svg";
+    else if (slider.value < 34) {
+        image.src = "./assets/media/icons/volume-level-1.svg";
    }
-   if (slider.value == 0) {
-    image.src = "https://github.com/jastakei/Lab5_PartyHorn/blob/cc84fdbae756b4be25f02f292bd96f78b867ee78/assets/media/icons/volume-level-0.svg";
+   else if (slider.value == 0) {
+        image.src = "./assets/media/icons/volume-level-0.svg";
+        hornbtn.disabled = true;
     }
-   // element.value = slider.value;
+    else {
+        image.src = "./assets/media/icons/volume-level-3.svg";
+    }
 }
 function updateNumber() {
     element.value = slider.value;
@@ -31,12 +43,29 @@ function updateNumber() {
     if (slider.value < 67) {
         image.src = "./assets/media/icons/volume-level-2.svg";
     }
-     if (slider.value < 34) {
-         image.src = "/Users/jasminetakei/Lab5_PartyHorn/assets/media/icons/volume-level-1.svg";
+     else if (slider.value < 34) {
+         image.src = "./assets/media/icons/volume-level-1.svg";
     }
-    if (slider.value == 0) {
-     image.src = "./media/icons/volume-level-0.svg";
+    else if (slider.value == 0) {
+         image.src = "./assets/media/icons/volume-level-0.svg";
+         hornbtn.disabled = true;
+
      }
+     else {
+         image.src = "./assets/media/icons/volume-level-3.svg";
+     }
+}
+function makeAir() {
+    mainImage.src = "./assets/media/images/air-horn.svg";
+    hornSound.src = "./assets/media/audio/air-horn.mp3";
+}
+function makeCar() {
+    mainImage.src = "./assets/media/images/car.svg";
+    hornSound.src = "./assets/media/audio/car-horn.mp3";
+}
+function makeParty() {
+    mainImage.src = "./assets/media/images/party-horn.svg";
+    hornSound.src = "./assets/media/audio/party-horn.mp3";
 }
 function playSound() {
     hornSound.play();
