94a88486c9024aaa6d83f4e6dba64decba6e074e
jastakei <jas.takei@gmail.com>
2021-02-04 00:41:16 -0800

    adding sound

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,12 +2,23 @@
 //const input = document.querySelector('input')
 var element = document.getElementById("volume-number");
 var slider = document.getElementById("volume-slider");
-element.addEventListener("input", updateValue);
+var hornbtn = document.getElementById("honk-btn");
+var hornSound = document.getElementById("horn-sound");
+element.addEventListener("input", updateSlider);
+slider.addEventListener("input", updateNumber);
+hornbtn.addEventListener("click", playSound);
 
-function updateValue() {
+
+function updateSlider() {
    // alert("it moved!!!");
    slider.value = element.value;
    // element.value = slider.value;
+}
+function updateNumber() {
+    element.value = slider.value;
+}
+function playSound() {
+    hornSound.play();
 }
 
 
